9c1cdbbb3572c97437133a8e0c68a00fccd0dde3
Mr Zayd <pcgamerjdid@gmail.com>
2026-02-21 17:53:56 +0000

    edit chat still neds icons

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import Chat from './Chat'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   
+      <Chat />
+  
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Chat.jsx
@@ -0,0 +1,13 @@
+export default function Chat() {
+  return (
+    <div className='w-full h-dvh bg-black m-0 justify-center items-center flex'>
+        <div className='bg-[#171717] w-2/5 h-110 rounded-[50px] border border-[#2D2D2D] '>
+        <select className='bg-[#101010] h-32 w-110 outline-none rounded-[20px] relative top-70 left-10'>
+         
+        </select>
+        
+        </div>
+      
+    </div>
+  )
+}
