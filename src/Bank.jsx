bb40c50aa1920cc1cac52606cd27fadb5f55b1f1
Mr Zayd <pcgamerjdid@gmail.com>
2026-02-22 03:38:31 +0000

    UPDATE 1.3

--- src/Bank.jsx
+++ src/Bank.jsx
@@ -9,8 +9,11 @@ export default function Bank({setBank}) {
     let value = e.target.value.replace(/\D/g, "");
 
     // add space every 4 digits
-    value = value.replace(/(.{4})/g, "$1 " ).trim();
-    value = value.replace(/(.{9})(.+)/g, "$1?$2" ).trim();
+
+
+    value = value.replace(/(.{4})/, "$1 " ).trim();
+ 
+   
     setCard(value);
   };
 
@@ -18,7 +21,7 @@ export default function Bank({setBank}) {
        <div className='absolute z-10 top-0 left-0 w-dvw h-dvh bg-black/80 flex justify-center items-center'>
         <div className=" w-1/4 h-1/3 bg-[#1B1B1B]  rounded-xl p-10 text-white">
         <h2 className='text-white'>Card Number</h2>
-        <input maxLength={69}  onChange={handleChange} value={card} className='bg-[#171717] w-4/5 h-7 rounded-[10px] border border-[#2D2D2D] ' type="text" />
+        <input maxLength={19}  onChange={handleChange} value={card} className='bg-[#171717] w-4/5 h-7 rounded-[10px] border border-[#2D2D2D] ' type="text" />
         <span className='flex w-full '><span>
             <h2>Expr Date</h2><input type="month" className='bg-[#171717] w-5/5 h-7 rounded-[10px] border border-[#2D2D2D] '  /></span><span><h2>CVV</h2><input className='bg-[#171717] w-5/5 h-7 rounded-[10px] border border-[#2D2D2D] ' type="password" /></span></span>
 
